82e1840f3b3b1da2183de7c26449fefee8267279
Vitor Lima <vitorauglima@gmail.com>
2026-02-18 14:30:15 -0300

    Adiciona aluno

---
+++ src/main/java/com/example/gestao_escola/model/Aluno.java
@@ -0,0 +1,41 @@
+package com.example.gestao_escola.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "alunos")
+public class Aluno {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    public Aluno() {
+    }
+
+    public Aluno(String nome) {
+        this.nome = nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
---
+++ src/main/java/com/example/gestao_escola/repository/AlunoRepository.java
@@ -0,0 +1,9 @@
+package com.example.gestao_escola.repository;
+
+import com.example.gestao_escola.model.Aluno;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AlunoRepository extends JpaRepository<Aluno, Long> {
+}
