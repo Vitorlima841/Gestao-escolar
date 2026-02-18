5bccaec9f2b29ff69b7335f243b2e34e3b79b252
Vitor Lima <vitorauglima@gmail.com>
2026-02-18 14:06:17 -0300

    commit inicial

---
+++ src/main/java/com/example/gestao_escola/GestaoEscolaApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestao_escola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoEscolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoEscolaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gestao_escola/GestaoEscolaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestao_escola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoEscolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
